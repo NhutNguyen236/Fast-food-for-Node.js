947fbbfe10b4f323c8b4dbaa01a5b27aee69f736
unknown <nhutnguyenf330@gmail.com>
2021-03-17 16:55:23 +0700

    :zap: update drawing in Chapter 04

--- Chapter04/express.js
+++ Chapter04/express.js
@@ -17,6 +17,31 @@ app.route('/sum/:a/:b').get((req, res) => {
     res.render('sum', {a: a, b:b, c: sum})
 })
 
+app.route('/draw/:row_number/:col_number/:symbol').get((req, res) => {
+    var s = req.params.symbol;
+    var rn = req.params.row_number;
+    var cn = req.params.col_number;
+    /**
+     * empty Array -> store 2 for loops
+     * we have an array with a picture inside
+     * parse array to string -> assign string to another variable
+     * assign the new variable ->  #{string}
+     */
+    var draw_arr = new Array();
+
+    for(var i = 0; i < 10; i ++){
+        draw_arr.push('\n');
+        for(var j = 0; j < 6; j++){
+            draw_arr.push(s);
+        }
+    }
+
+    var draw_str = draw_arr.toString();
+    console.log(draw_str);
+
+    res.render('draw', {row_number: rn, col_number: cn, symbol: s});
+})
+
 var server = app.listen(8080, () =>{
     console.log("The server is now running at http://localhost:8080");
 })
